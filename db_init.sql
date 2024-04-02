b9f95d54c3237cb8f41a9509d12111fcd2f1eb1e
Mohammadreza Beygifard <samanbeygifard73@gmail.com>
2024-04-02 15:58:21 +0200

    Change db_init

--- db_init.sql
+++ db_init.sql
@@ -7,13 +7,4 @@ CREATE TABLE "role_entity" (
     name VARCHAR(255) NOT NULL
 );
 
-INSERT INTO "role_entity" (name) VALUES ('ADMIN');
 INSERT INTO "role_entity" (name) VALUES ('USER');
-
--- CREATE TABLE todo_table (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     user_id INT NOT NULL,
---     task VARCHAR(255) NOT NULL,
---     completed BOOLEAN NOT NULL,
---     FOREIGN KEY (user_id) REFERENCES user_table(id)
--- );
